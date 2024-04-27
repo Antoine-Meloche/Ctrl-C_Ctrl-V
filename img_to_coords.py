b01e7a346749b77f960e59f07588113690b35603
Antoine Meloche <antoine@pop-os.uqo.local>
2024-04-26 22:34:25 -0400

    initial commit

---
+++ img_to_coords.py
@@ -0,0 +1,30 @@
+import exiftool
+from ultralytics import YOLO
+import requests
+
+# model = YOLO("model.pt")
+# results = model("img-1.jpg")
+
+def find_size(cam_distance, pixels, focal_length):
+	return pixels * cam_distance / focal_length
+
+file = "img-1.jpg"
+with exiftool.ExifToolHelper() as et:
+    metadata = et.get_metadata(file)
+    
+    focal_length = metadata[0]['EXIF:FocalLength']
+    subject_distance = metadata[0]['EXIF:SubjectDistance']
+    lat_ref = metadata[0]['EXIF:GPSLatitudeRef']
+    latitude = metadata[0]['EXIF:GPSLatitude']
+    lon_ref = metadata[0]['EXIF:GPSLongitudeRef']
+    longitude = metadata[0]['EXIF:GPSLongitude']
+
+    # print(metadata[0])
+
+    print((-1 if lat_ref == 'S' else 1) * latitude)
+    print((-1 if lon_ref == 'W' else 1) * longitude)
+    print(find_size(subject_distance, 100, focal_length))
+
+url = f"https://nominatim.openstreetmap.org/reverse?lat={(-1 if lat_ref == 'S' else 1) * latitude}&lon={(-1 if lon_ref == 'W' else 1) * longitude}&format=json"
+
+print(requests.get(url).json()["address"]["road"])
